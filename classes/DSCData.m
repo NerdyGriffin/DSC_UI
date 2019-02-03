211850e16a255b3c530704b0725a33fd283f1897
Christian Kunis <ckunis98@gmail.com>
2019-02-03 17:29:31 -0500

    Fixed DSCData load .mat file function

--- classes/DSCData.m
+++ classes/DSCData.m
@@ -628,6 +628,10 @@ classdef DSCData < handle
                     % Load the .mat files into a temporary struct variable
                     S = load(dataFullPath);
                     
+                    if ~isfield(S, 'dscData')
+                        error('The selected .mat file does not contain a DSCData object.')
+                    end
+                    
                     % Get each of the properties from the temporary struct
                     % and store them in the appropriate properties
                     obj.ReferenceSampleData = S.dscData.ReferenceSampleData;
